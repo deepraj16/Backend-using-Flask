bbc9b9255b3c8c552477515b722fd3b10f3b5914
Deepraj Autade <149596333+deepraj16@users.noreply.github.com>
2025-10-14 12:16:20 +0530

    Update indexing.py
    Signed-off-by: Deepraj Autade <149596333+deepraj16@users.noreply.github.com>

--- 1-Prathmesh/indexing.py
+++ 1-Prathmesh/indexing.py
@@ -2,9 +2,10 @@ import os
 from langchain_mistralai.chat_models import ChatMistralAI
 
 def setup_llm(): 
-    llm = ChatMistralAI(api_key="lHcwga2vJ6yyjV470WdMIFn5hRgtMbcc")
+    llm = ChatMistralAI(api_key="")
     return llm
 
 
 model=setup_llm()
 print(model.invoke("Hello! How can I assist you today?"))
+
